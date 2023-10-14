aae6660b133c805909a0566142ee84570748acc2
Ashvin Valentine <ashvin.valentine281@gmail.com>
2023-10-14 15:51:16 +0100

    Add models

--- things/models.py
+++ things/models.py
@@ -5,4 +5,4 @@ from django.db.models import Model
 class Thing(Model):
     name = models.TextField(unique=True, max_length=30)
     description = models.TextField(unique=False, max_length=120, blank=False)
-    quantity = models.IntegerField(unique=False, max_length=100)
+    quantity = models.IntegerField(unique=False)

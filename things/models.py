9f8e4d53f353af0f508b314e3855876d828967b0
Ashvin Valentine <ashvin.valentine281@gmail.com>
2023-10-14 15:54:44 +0100

    Make migrations

--- things/models.py
+++ things/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.db.models import Model
 
 # Create your models here.
-class Thing(Model):
-    name = models.TextField(unique=True, max_length=30)
+class Thing(models.Model):
+    name = models.CharField(unique=True, max_length=30)
     description = models.TextField(unique=False, max_length=120, blank=False)
     quantity = models.IntegerField(unique=False)
